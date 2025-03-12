c5d7985d582f94585e3f10e496352470c447a8c6
Atherizz <saveroathallah86@gmail.com>
2025-03-12 14:59:25 +0700

    belajar stack

---
+++ leetcode/valid_parentheses.go
@@ -0,0 +1,61 @@
+package main
+
+type Stack struct {
+	items []rune
+}
+
+func (s *Stack) Push(item rune) {
+	s.items = append(s.items, item)
+}
+
+func (s *Stack) Pop() rune {
+	if len(s.items) == 0 {
+		return 0
+	} else {
+		item := s.items[len(s.items)-1]
+		s.items = s.items[:len(s.items)-1]
+		return item
+	}
+}
+
+func (s *Stack) Peek() rune {
+	if len(s.items) == 0 {
+		return -1
+	} else {
+		item := s.items[len(s.items)-1]
+		return item
+	}
+}
+
+func (s *Stack) IsEmpty() bool {
+	return len(s.items) == 0
+}
+
+func (s *Stack) Size() int {
+	return len(s.items)
+}
+
+func isValid(s string) bool {
+	data := Stack{
+		items: []rune{},
+	}
+
+	for _, char := range s {
+		if char == '(' || char == '{' || char == '[' {
+			data.Push(char)
+			continue
+		}
+		if len(data.items) == 0 {
+			return false
+		}
+		close := data.Peek()
+		if close == '(' && char == ')' || close == '{' && char == '}' || close == '[' && char == ']' {
+			data.Pop()
+		} else {
+			return false
+		}
+	}
+
+	return data.IsEmpty()
+}
+
